fc38d39e4b44adba2e28715c8995352e334dccdc
MasterMindsIT <ronaldgarciavazquez@gmail.com>
2025-05-31 10:49:00 -0400

    Done bug hero buttons

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [TranslateModule,CommonModule],
+  imports: [TranslateModule, CommonModule, RouterModule, MatButtonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })

90ce504aca28da29f783229fa103478a7396d2bf
MasterMindsIT <ronaldgarciavazquez@gmail.com>
2025-06-10 10:38:33 -0400

    Complete url footer and translate json

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, MatDividerModule,TranslateModule],
+  imports: [CommonModule, MatDividerModule,TranslateModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
